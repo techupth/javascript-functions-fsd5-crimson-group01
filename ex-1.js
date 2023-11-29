3fd3446af0cd209e0d94c6bf1b41b286f136cbea
bodin <bodinsonsawan@gmail.com>
2023-11-29 11:43:51 +0700

    fix(ex1-6):bodin's assignment

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,19 @@
-// Start coding here
+function add(a, b) {
+  return a + b;
+}
+console.log(add(10, 5));
+
+let subtract = (a, b) => {
+  return a - b;
+};
+console.log(subtract(10, 5));
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(10, 5));
+
+let divide = (a, b) => {
+  return a / b;
+};
+console.log(divide(10, 5));
